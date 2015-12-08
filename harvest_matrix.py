acb8a97d7fb1327ca20d3a74dd5ed59dd3991c04
Chris Snyder <chris@chrissnyder.org>
2015-12-07 22:18:59 -0500

    More

--- harvest_matrix.py
+++ harvest_matrix.py
@@ -2,11 +2,16 @@
 from rgbmatrix import RGBMatrix
 import time
 import harvest
-
-
-
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('company_name')
+    parser.add_argument('email')
+    parser.add_argument('password')
+    options = parser.parse_args()
+
+    print(options)
     # 1. Start display process
 
     # 2. Start geting harvest information
