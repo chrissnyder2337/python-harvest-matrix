89e5f08b2cdbeee619a294c8cece0fb5b1e36d1e
Chris Snyder <chris@chrissnyder.org>
2015-12-07 22:55:48 -0500

    more

--- harvest_matrix.py
+++ harvest_matrix.py
@@ -1,8 +1,60 @@
 #!/usr/bin/env python
 from rgbmatrix import RGBMatrix
+from rgbmatrix import graphics
 import time
 import harvest
+import multiprocessing
 import argparse
+
+
+class Matrix(object):
+    STATE_STOPPED = 0
+    STATE_STARTED = 1
+    STATE_NO_TASK = 2
+    STATE_TASK_RUNNING = 3
+
+    def __init__(self):
+        self.displayState = self.STATE_STOPPED
+        self.projectCode = ''
+        self.taskHours = 0.0
+        self.width = 32
+        self.height = 16
+        self.matrix = RGBMatrix(self.height, 1, 1)
+
+    def run(self):
+        while True:
+            if self.displayState == self.STATE_STOPPED:
+                print("Run:State Stopped")
+                canvas = self.matrix.CreateFrameCanvas()
+                font = graphics.Font()
+                font.LoadFont("./rpi-rgb-led-matrix/fonts/5x7.bdf")
+                textColor = graphics.Color(255, 255, 0)
+                myText = "stopped"
+                graphics.DrawText(canvas, font, 0, 10, textColor, myText);
+                self.matrix.SwapOnVSync(canvas)
+            elif self.displayState == self.STATE_STARTED:
+                print("Run:State Stopped")
+                canvas = self.matrix.CreateFrameCanvas()
+                font = graphics.Font()
+                font.LoadFont("./rpi-rgb-led-matrix/fonts/5x7.bdf")
+                textColor = graphics.Color(255, 255, 0)
+                myText = "Started"
+                graphics.DrawText(canvas, font, 0, 10, textColor, myText);
+                self.matrix.SwapOnVSync(canvas)
+            elif self.displayState == self.STATE_NO_TASK:
+                pass
+            elif self.displayState == self.STATE_TASK_RUNNING:
+                pass
+
+
+def matrix_process():
+    matrix = Matrix()
+
+    while True:
+        matrix.run()
+
+
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,11 +62,19 @@ def main():
     parser.add_argument('email')
     parser.add_argument('password')
     options = parser.parse_args()
-
     print(options)
+
     # 1. Start display process
+    matrixProcess = multiprocessing.Process(target=matrix_process)
+    matrixProcess.start()
+
+
+
+
+
 
     # 2. Start geting harvest information
+
 
 if __name__ == "__main__":
     main()
